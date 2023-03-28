ede1d032f97edf0b6dc51832d7107904847ed1b1
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-03-27 23:52:01 -0700

    Finish 6.22

---
+++ query-anecdotes/src/App.jsx
@@ -0,0 +1,47 @@
+import AnecdoteForm from './components/AnecdoteForm';
+import Notification from './components/Notification';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { getAnecdotes, updateAnecdote } from './requests';
+
+const App = () => {
+	const queryClient = useQueryClient();
+	const updateAnecdoteMutation = useMutation(updateAnecdote, {
+		onSuccess: () => queryClient.invalidateQueries('anecdotes'),
+	});
+
+	const result = useQuery('anecdotes', getAnecdotes, {
+		refetchOnWindowFocus: false,
+	});
+	console.log(result);
+
+	if (result.isLoading) {
+		return <div>Loading...</div>;
+	}
+	const anecdotes = result.data;
+
+	const handleVote = (anecdote) => {
+		console.log('vote');
+		updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 });
+	};
+
+	return (
+		<div>
+			<h3>Anecdote app</h3>
+
+			<Notification />
+			<AnecdoteForm />
+
+			{anecdotes.map((anecdote) => (
+				<div key={anecdote.id}>
+					<div>{anecdote.content}</div>
+					<div>
+						has {anecdote.votes}
+						<button onClick={() => handleVote(anecdote)}>vote</button>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default App;
